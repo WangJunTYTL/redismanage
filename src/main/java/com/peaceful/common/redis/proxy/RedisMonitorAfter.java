c7a5682fc7783a1facdf2f29765c2e311d8688f1
wangjun <wangjun@edaijia-inc.cn>
2016-07-22 13:40:16 +0800

    not find perf4j 2.0 version

--- src/main/java/com/peaceful/common/redis/proxy/RedisMonitorAfter.java
+++ src/main/java/com/peaceful/common/redis/proxy/RedisMonitorAfter.java
@@ -19,8 +19,7 @@ public class RedisMonitorAfter implements RedisInvokeAop {
         StopWatch stopWatch = (StopWatch) invokeContext.get("redis.stopWatch");
         long cost = stopWatch.getElapsedTime();
         String node = (String) invokeContext.get("redis.node");
-        stopWatch.stop("REDIS." + node);
-        stopWatch.stop("REDIS");
+        stopWatch.stop("REDIS." + node.toUpperCase());
         if (cost > 500) {
             logger.warn("slow cmd: {}{} cost {} ms at {} node , result {}", invokeContext.get("redis.cmd"), invokeContext.get("redis.args"), cost, invokeContext.get("redis.node"), invokeContext.get("result"));
         } else {
